55f0f46c9fda4e0e4ec0c52e1bb5e694fd0aef30
amsa-21 <amsata2009@gmail.com>
2024-09-18 11:20:41 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Connexion from "./layout/Connexion";
 import Subscription from "./layout/Subscription";
 import ExternalSign from "./layout/ExternalSign";
 import routes from "./routes";
 
-const renderRoutes = () => {
-  const navigate = useNavigate();
-  return routes.map((route) => (
-    <Route
-      key={route.key}
-      path={route.path}
-      element={
-        localStorage.getItem("accessToken") !== null
-          ? route.component
-          : navigate("/login")
-      }
-    />
-  ));
-};
-
 function App() {
-  const navigate = useNavigate();
   return (
     <Routes>
       <Route path="/login" element={<Connexion />} />
@@ -30,8 +14,20 @@ function App() {
         path="/extSign/:name/:doc/:refreshToken"
         element={<ExternalSign />}
       />
-      {renderRoutes()}
-      <Route path="*" element={navigate("/login")} />
+      {routes.map((route) => (
+        <Route
+          key={route.key}
+          path={route.path}
+          element={
+            localStorage.getItem("accessToken") !== null ? (
+              route.component
+            ) : (
+              <Connexion />
+            )
+          }
+        />
+      ))}
+      <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
 }
